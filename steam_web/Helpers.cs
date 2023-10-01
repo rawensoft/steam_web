a214665d45eb9f53f21ff42c501c8929c4a2f91a
rawensoft <rawensoft@ya.ru>
2023-10-01 13:25:19 +0300

    Удалена замена символов при открытии ссылки в windows

--- steam_web/Helpers.cs
+++ steam_web/Helpers.cs
@@ -14,7 +14,6 @@ public static class Helpers
             // hack because of this: https://github.com/dotnet/corefx/issues/10361
             if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
             {
-                url = url.Replace("&", "^&");
                 System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo() { UseShellExecute = true, FileName = url });
             }
             else if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Linux))
