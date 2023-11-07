bb64b29cf8ae3ef7d8e1400623350833183777a4
rawensoft <rawensoft@ya.ru>
2023-11-07 16:53:13 +0300

    Добавлена фабрика для хранения и создания RestClient для запросов

---
+++ steam_web/Web/Models/ClientIndexData.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace SteamWeb.Web.Models;
+
+readonly struct ClientIndexData
+{
+    public int UriHash { get; }
+    public int ProxyHash { get; }
+    public int UserAgent { get; }
+
+    public ClientIndexData(string url, IWebProxy? proxy, string ua) : this(new Uri(url), proxy, ua) { }
+    public ClientIndexData(Uri uri, IWebProxy? proxy, string ua)
+    {
+        UriHash = uri.Authority.GetHashCode();
+        ProxyHash = proxy?.GetProxy(uri)?.GetHashCode() ?? -1;
+        UserAgent = ua.GetHashCode();
+    }
+}
---
+++ steam_web/Web/RestClientFactory.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using RestSharp;
+using System.Collections.Concurrent;
+using SteamWeb.Web.Models;
+
+namespace SteamWeb.Web;
+
+static class RestClientFactory
+{
+	private static readonly ConcurrentDictionary<ClientIndexData, RestClient> _clients = new();
+
+	/// <summary>
+	/// Находит RestClient по proxy, url (domain) и user agent
+	/// </summary>
+	/// <returns>Возвращает RestClient</returns>
+	public static RestClient GetClient(string url, IWebProxy? proxy, string ua) => GetClient(new Uri(url), proxy, ua);
+	public static RestClient GetClient(Uri uri, IWebProxy? proxy, string ua)
+	{
+        var indexData = new ClientIndexData(uri, proxy, ua);
+		RestClient? client = null;
+		_clients!.AddOrUpdate(indexData, (key) =>
+		{
+			client = new RestClient(new RestClientOptions()
+			{
+				AutomaticDecompression = DecompressionMethods.All,
+				FollowRedirects = false,
+				UserAgent = ua,
+				BaseUrl = new Uri(uri.Scheme + "://" + uri.Host),
+				AllowMultipleDefaultParametersWithSameName = true,
+				Proxy = proxy,
+				// если скрыть, то при Redirect куки сбрасываются
+				CookieContainer = null, // из-за этого происходит дублирование кук и если какие-то куки изменились, то будут изменённые и не изменённые
+				ConfigureMessageHandler = h =>
+				{
+					((HttpClientHandler)h).UseCookies = true; // если это не установить, то Set-Cookie не работает
+															  // если скрыть, то куки записываются в этот контейнер, в который мы не можем попасть
+					((HttpClientHandler)h).CookieContainer = new(); // если это не установить, то куки из Set-Cookie не добавляются в наш CookieContainer
+					((HttpClientHandler)h).UseProxy = proxy != null;
+					((HttpClientHandler)h).Proxy = proxy;
+					return h;
+				}
+			});
+			return client!;
+		},
+		(key, oldValue) =>
+		{
+            client = oldValue;
+			return oldValue;
+		});
+        return client!;
+	}
+}
