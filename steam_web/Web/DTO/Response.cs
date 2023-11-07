56ab45520bf638be233353ea7ae889d5d0480f64
rawensoft <rawensoft@ya.ru>
2023-11-07 17:00:34 +0300

    Добавлена обработка CookieCollection в Response

--- steam_web/Web/DTO/Response.cs
+++ steam_web/Web/DTO/Response.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using SteamWeb.Extensions;
 using RestSharp;
+using System.Net.Sockets;
 
 namespace SteamWeb.Web.DTO;
 
@@ -13,97 +14,132 @@ public class Response
 
     public CookieContainer? CookieContainer { get; set; } = null;
     public bool Success { get; init; } = false;
-    public string? Cookie { get; init; } = null;
+    public string? Cookie { get; private set; } = null;
     public int StatusCode { get; init; } = 0;
     public Exception? ErrorException { get; init; } = null;
     public string? ErrorMessage { get; init; } = null;
     public bool LostAuth { get; init; } = false;
     public EResult EResult { get; init; } = EResult.Invalid;
+	public SocketError SocketErrorCode { get; private set; } = SocketError.Success;
 
-    public Response(RestResponse res) : this(res, null) { }
-    public Response(RestResponse res, CookieContainer? cookies)
-    {
-        Success = res.IsSuccessful;
-        StatusCode = (int)res.StatusCode;
-        if (!Success && res.StatusCode == HttpStatusCode.Found)
-        {
-            foreach (var header in res.Headers!)
-            {
-                switch (header.Name)
-                {
-                    case HeaderNameLocation:
-                        if (header.Value!.ToString() == HeaderValueLocation)
-                            LostAuth = true;
-                        break;
-                    case HeaderNameXEResult:
-                        if (header.Value != null)
-                            EResult = (EResult)header.Value.ToString()!.ParseInt32();
-                        break;
-                }
-            }
-        }
-        else if (res.Headers != null)
-        {
-            foreach (var header in res.Headers)
-            {
-                if (header.Name == HeaderNameXEResult && header.Value != null)
-                {
-                    EResult = (EResult)header.Value.ToString()!.ParseInt32();
-                    break;
-                }
-            }
-        }
-        if (!Success)
-        {
-            ErrorException = res.ErrorException;
-            ErrorMessage = res.ErrorMessage;
-        }
-        if (cookies != null)
-        {
-            var sb = new StringBuilder();
-            foreach (Cookie cookie in cookies.GetAllCookies())
-            {
-                sb.Append(cookie.Name);
-                sb.Append('=');
-                sb.Append(cookie.Value);
-                sb.Append("; ");
-            }
-            Cookie = sb.ToString();
-        }
-        CookieContainer = cookies;
-    }
-    public Response(HttpWebResponse res)
-    {
-        if (res == null)
-            return;
-        StatusCode = (int)res.StatusCode;
-        Success = StatusCode >= 200 && StatusCode < 300;
-        if (!Success && res.StatusCode == HttpStatusCode.Found)
-        {
-            foreach (string name in res.Headers.AllKeys)
-            {
-                switch (name)
-                {
-                    case HeaderNameLocation:
-                        if (res.Headers[HeaderNameLocation] == HeaderValueLocation)
-                            LostAuth = true;
-                        break;
-                    case HeaderNameXEResult:
-                        EResult = (EResult)res.Headers[HeaderNameLocation]!.ParseInt32();
-                        break;
-                }
-            }
-        }
-        else if (res.Headers != null)
-        {
-            foreach (string name in res.Headers.AllKeys)
-            {
-                if (name == HeaderNameXEResult)
-                {
-                    EResult = (EResult)res.Headers[HeaderNameXEResult]!.ParseInt32();
-                    break;
-                }
-            }
-        }
-    }
+	public Response(RestResponse res)
+	{
+		Success = res.IsSuccessful;
+		StatusCode = (int)res.StatusCode;
+		if (!Success && res.StatusCode == HttpStatusCode.Found)
+		{
+			foreach (var header in res.Headers!)
+			{
+				switch (header.Name)
+				{
+					case HeaderNameLocation:
+						if (header.Value!.ToString() == HeaderValueLocation)
+							LostAuth = true;
+						break;
+					case HeaderNameXEResult:
+						if (header.Value != null)
+							EResult = (EResult)header.Value.ToString()!.ParseInt32();
+						break;
+				}
+			}
+		}
+		else if (res.Headers != null)
+		{
+			foreach (var header in res.Headers)
+			{
+				if (header.Name == HeaderNameXEResult && header.Value != null)
+				{
+					EResult = (EResult)header.Value.ToString()!.ParseInt32();
+					break;
+				}
+			}
+		}
+		if (!Success)
+		{
+			if (res.ErrorException != null && res.ErrorException.InnerException != null)
+			{
+				ErrorException = res.ErrorException.InnerException;
+				ErrorMessage = res.ErrorException.InnerException.Message;
+				var socketEx = ErrorException as SocketException;
+				if (socketEx != null)
+					SocketErrorCode = socketEx.SocketErrorCode;
+			}
+			else
+			{
+				ErrorException = res.ErrorException;
+				ErrorMessage = res.ErrorMessage;
+			}
+		}
+	}
+	public Response(HttpWebResponse res)
+	{
+		if (res == null)
+			return;
+		StatusCode = (int)res.StatusCode;
+		Success = StatusCode >= 200 && StatusCode < 300;
+		if (!Success && res.StatusCode == HttpStatusCode.Found)
+		{
+			foreach (string name in res.Headers.AllKeys)
+			{
+				switch (name)
+				{
+					case HeaderNameLocation:
+						if (res.Headers[HeaderNameLocation] == HeaderValueLocation)
+							LostAuth = true;
+						break;
+					case HeaderNameXEResult:
+						EResult = (EResult)res.Headers[HeaderNameLocation]!.ParseInt32();
+						break;
+				}
+			}
+		}
+		else if (res.Headers != null)
+		{
+			foreach (string name in res.Headers.AllKeys)
+			{
+				if (name == HeaderNameXEResult)
+				{
+					EResult = (EResult)res.Headers[HeaderNameXEResult]!.ParseInt32();
+					break;
+				}
+			}
+		}
+	}
+	public Response(RestResponse res, CookieContainer? cookies) : this(res) => WriteCookies(cookies);
+	public Response(RestResponse res, CookieCollection? cookies) : this(res) => WriteCookies(cookies);
+
+	private void WriteCookies(CookieContainer? cookies)
+	{
+		if (cookies != null)
+		{
+			var sb = new StringBuilder();
+			foreach (Cookie cookie in cookies.GetAllCookies())
+			{
+				sb.Append(cookie.Name);
+				sb.Append('=');
+				sb.Append(cookie.Value);
+				sb.Append("; ");
+			}
+			Cookie = sb.ToString();
+		}
+		CookieContainer = cookies;
+	}
+	private void WriteCookies(CookieCollection? cookies)
+	{
+		if (cookies != null)
+		{
+			var container = new CookieContainer();
+			var sb = new StringBuilder();
+			foreach (Cookie cookie in cookies)
+			{
+				sb.Append(cookie.Name);
+				sb.Append('=');
+				sb.Append(cookie.Value);
+				sb.Append("; ");
+				container.Add(new Uri("https://" + cookie.Domain), cookie);
+			}
+			Cookie = sb.ToString();
+			CookieContainer = container;
+		}
+	}
 }
--- steam_web/Web/DTO/StringResponse.cs
+++ steam_web/Web/DTO/StringResponse.cs
@@ -7,8 +7,9 @@ public class StringResponse : Response
 {
     public string? Data { get; init; } = null;
     public StringResponse(RestResponse res) : base(res) => Data = res.Content;
-    public StringResponse(RestResponse res, CookieContainer cookies) : base(res, cookies) => Data = res.Content;
-    public StringResponse(HttpWebResponse res) : base(res)
+    public StringResponse(RestResponse res, CookieContainer? cookies) : base(res, cookies) => Data = res.Content;
+	public StringResponse(RestResponse res, CookieCollection? cookies) : base(res, cookies) => Data = res.Content;
+	public StringResponse(HttpWebResponse res) : base(res)
     {
         if (res != null && res.ContentLength > 0)
         {
