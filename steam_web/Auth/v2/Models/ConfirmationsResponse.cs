e22812028761b90d08fdfd628a10725b6039cc84
rawensoft <rawensoft@ya.ru>
2023-12-28 16:36:05 +0300

    Добавлен индексатор для класса подтверждений

--- steam_web/Auth/v2/Models/ConfirmationsResponse.cs
+++ steam_web/Auth/v2/Models/ConfirmationsResponse.cs
@@ -7,5 +7,11 @@ public class ConfirmationsResponse
     public string message { get; init; }
     public string detail { get; init; }
     public Confirmation[] conf { get; init; } = new Confirmation[0];
+    
+    public Confirmation this[int index]
+	{
+		get => conf[index];
+		set => conf[index] = value;
+	}
 }
 
