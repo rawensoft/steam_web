80dafa0c10a193d3afe81cd5524e652cf047648e
rawensoft <rawensoft@ya.ru>
2023-12-28 16:50:02 +0300

    Добавлен класс для запроса на подтверждение входа по Qr

---
+++ steam_web/Auth/v2/DTO/CAuthentication_UpdateAuthSessionWithMobileConfirmation_Request.cs
@@ -0,0 +1,20 @@
+﻿using ProtoBuf;
+
+namespace SteamWeb.Auth.v2.DTO;
+
+[ProtoContract]
+public class CAuthentication_UpdateAuthSessionWithMobileConfirmation_Request
+{
+    [ProtoMember(1)]
+    public short version { get; set; }
+    [ProtoMember(2)]
+    public ulong client_id { get; set; }
+    [ProtoMember(3, DataFormat = DataFormat.FixedSize)]
+    public ulong steamid { get; set; }
+    [ProtoMember(4)]
+    public byte[] signature { get; set; }
+    [ProtoMember(5)]
+    public bool confirm { get; set; }
+    [ProtoMember(6)]
+    public ESessionPersistence persistence { get; set; }
+}
