08203453e9e9234c23dda342a85a5388c13454dd
rawensoft <rawensoft@ya.ru>
2023-12-28 16:57:34 +0300

    Добавлен класс для получения массива байтов с запроса

---
+++ steam_web/Web/DTO/ByteResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Text;
+using RestSharp;
+
+namespace SteamWeb.Web.DTO;
+
+public class ByteResponse : Response
+{
+	public byte[]? Data { get; init; } = null;
+	public ByteResponse(RestResponse res) : base(res) => Data = res.RawBytes;
+	public ByteResponse(RestResponse res, CookieContainer? cookies) : base(res, cookies) => Data = res.RawBytes;
+	public ByteResponse(RestResponse res, CookieCollection? cookies) : base(res, cookies) => Data = res.RawBytes;
+	public ByteResponse(HttpWebResponse res) : base(res)
+	{
+		if (res != null && res.ContentLength > 0)
+		{
+			using var sr = new StreamReader(res.GetResponseStream());
+			Data = Encoding.UTF8.GetBytes(sr.ReadToEnd());
+		}
+	}
+}
