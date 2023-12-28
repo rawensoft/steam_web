cef4b8185ca17b6f33a24cacef47fe11c230061a
rawensoft <rawensoft@ya.ru>
2023-12-28 16:35:25 +0300

    Добавлен enum для указывания типа обновления при обновлении access_token

---
+++ steam_web/Auth/v2/Enums/ETokenRenewalType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamWeb.Auth.v2.Enums;
+public enum ETokenRenewalType
+{
+	k_ETokenRenewalType_None = 0,
+	k_ETokenRenewalType_Allow = 1
+}
