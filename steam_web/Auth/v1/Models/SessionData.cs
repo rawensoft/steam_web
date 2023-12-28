5bae83b6bdf3496899e8c359be53cd3e44dbc16b
rawensoft <rawensoft@ya.ru>
2023-12-28 15:50:05 +0300

    Удалёние не рабочего кода авторизации версии 1

--- steam_web/Auth/v1/Models/SessionData.cs
+++ steam_web/Auth/v1/Models/SessionData.cs
@@ -1,21 +1,16 @@
-﻿using SteamWeb.Auth.v1.Enums;
-using SteamWeb.Extensions;
+﻿using SteamWeb.Extensions;
 using System.Net;
 using SteamWeb.Auth.Interfaces;
+using System.Text.Json.Serialization;
+using System.Text;
 
 namespace SteamWeb.Auth.v1.Models;
 public class SessionData : ISessionProvider
 {
-    private string sessionID;
-    private string steamLogin;
-    private string steamLoginSecure;
-    private string steamMachineAuth;
-    private string steamRememberLogin;
-    private string oAuthToken;
-    private string webCookie;
-    private string steamCountry;
-    private string browserID;
-    private string steamLanguage = "english";
+	private string? _sessionID;
+	private string? _accessToken;
+	private string? _refreshToken;
+	private string _steamLanguage = "english";
 
     /// <summary>
     /// Вызывается при обновлении данных сессии
@@ -26,114 +21,58 @@ public class SessionData : ISessionProvider
     /// Вызывается при обновлении данных сессии
     /// </summary>
     public event OnSessionUpdatedHandler OnSessionUpdated;
-    public string SessionID
+    
+    [JsonIgnore] public string SteamLanguage
     {
-        get => sessionID;
+        get => _steamLanguage;
         set
         {
-            if (sessionID == value) return;
-            sessionID = value;
+            if (_steamLanguage == value) return;
+			_steamLanguage = value;
             OnSessionUpdated?.Invoke(this);
         }
     }
-    public string SteamLogin
-    {
-        get => steamLogin;
-        set
-        {
-            if (steamLogin == value) return;
-            steamLogin = value;
-            OnSessionUpdated?.Invoke(this);
-        }
-    }
-    public string SteamLoginSecure
-    {
-        get => steamLoginSecure;
-        set
-        {
-            if (steamLoginSecure == value) return;
-            steamLoginSecure = value;
-            OnSessionUpdated?.Invoke(this);
-        }
-    }
-    public string SteamMachineAuth
-    {
-        get => steamMachineAuth;
-        set
-        {
-            if (steamMachineAuth == value) return;
-            steamMachineAuth = value;
-            OnSessionUpdated?.Invoke(this);
-        }
-    }
-    public string SteamRememberLogin
-    {
-        get => steamRememberLogin;
-        set
-        {
-            if (steamRememberLogin == value) return;
-            steamRememberLogin = value;
-            OnSessionUpdated?.Invoke(this);
-        }
-    }
-    public string OAuthToken
-    {
-        get => oAuthToken;
-        set
-        {
-            if (oAuthToken == value) return;
-            oAuthToken = value;
-            OnSessionUpdated?.Invoke(this);
-        }
-    }
-    public string WebCookie
-    {
-        get => webCookie;
-        set
-        {
-            if (webCookie == value) return;
-            webCookie = value;
-            OnSessionUpdated?.Invoke(this);
-        }
-    }
-    public string SteamCountry
-    {
-        get => steamCountry;
-        set
-        {
-            if (steamCountry == value) return;
-            steamCountry = value;
-            OnSessionUpdated?.Invoke(this);
-        }
-    }
-    public string BrowserID
-    {
-        get => browserID;
-        set
-        {
-            if (browserID == value) return;
-            browserID = value;
-            OnSessionUpdated?.Invoke(this);
-        }
-    }
-    public string SteamLanguage
-    {
-        get => steamLanguage;
-        set
-        {
-            if (steamLanguage == value) return;
-            steamLanguage = value;
-            OnSessionUpdated?.Invoke(this);
-        }
-    }
-    public ulong SteamID { get; set; }
-    public SignInPlatform Platform { get; init; } = SignInPlatform.Mobile;
+	public string SessionID
+	{
+		get => _sessionID;
+		set
+		{
+			if (_sessionID == value)
+				return;
+			_sessionID = value;
+			OnSessionUpdated?.Invoke(this);
+		}
+	}
+	public string AccessToken
+	{
+		get => _accessToken;
+		set
+		{
+			if (_accessToken == value)
+				return;
+			_accessToken = value;
+			OnSessionUpdated?.Invoke(this);
+		}
+	}
+	public string RefreshToken
+	{
+		get => _refreshToken;
+		set
+		{
+			if (_refreshToken == value)
+				return;
+			_refreshToken = value;
+			OnSessionUpdated?.Invoke(this);
+		}
+	}
+	public ulong SteamID { get; set; }
 
-    /// <summary>
-    /// Добавляет мобильные куки
-    /// </summary>
-    /// <param name="cookies">Куда добавлять</param>
-    private void AddCookies(CookieContainer? container, Uri url)
+
+	/// <summary>
+	/// Добавляет мобильные куки
+	/// </summary>
+	/// <param name="cookies">Куда добавлять</param>
+	private void AddCookies(CookieContainer? container, Uri url)
     {
         if (container == null)
             container = new();
@@ -145,17 +84,11 @@ public class SessionData : ISessionProvider
             case "api.steampowered.com":
             case "steampowered.com":
                 container.Add(url, new Cookie("mobileClient", "android") { Secure = true });
-                container.Add(url, new Cookie("mobileClientVersion", "0 (2.1.3)") { Secure = true });
+                container.Add(url, new Cookie("mobileClientVersion", "777777 3.6.4") { Secure = true });
                 container.Add(url, new Cookie("Steam_Language", SteamLanguage) { Secure = true });
-                container.Add(url, new Cookie("dob", "") { Secure = true });
-                if (SteamID > 0)
-                    container.Add(url, new Cookie("steamid", SteamID.ToString()) { Secure = true });
-                if (!SessionID.IsEmpty())
+				container.Add(url, new Cookie("steamLoginSecure", GetSteamLoginSecure()) { Secure = true, HttpOnly = true });
+				if (!SessionID.IsEmpty())
                     container.Add(url, new Cookie("sessionid", SessionID) { Secure = true });
-                if (!SteamLogin.IsEmpty())
-                    container.Add(url, new Cookie("steamLogin", SteamLogin) { HttpOnly = true });
-                if (!SteamLoginSecure.IsEmpty())
-                    container.Add(url, new Cookie("steamLoginSecure", SteamLoginSecure) { Secure = true, HttpOnly = true });
                 break;
         }
     }
@@ -164,45 +97,25 @@ public class SessionData : ISessionProvider
     {
         if (container == null)
             container = new();
-        if (Platform == SignInPlatform.Mobile)
-            AddCookies(container, url);
-        else
-        {
-            switch (url.Host)
-            {
-                case "steamcommunity.com":
-                case "store.steampowered.com":
-                case "help.steampowered.com":
-                case "api.steampowered.com":
-                case "steampowered.com":
-                    container.Add(url, new Cookie("Steam_Language", SteamLanguage) { Secure = true });
-                    if (!SessionID.IsEmpty())
-                        container.Add(url, new Cookie("sessionid", SessionID) { Secure = true });
-                    if (!SteamLoginSecure.IsEmpty())
-                        container.Add(url, new Cookie("steamLoginSecure", SteamLoginSecure) { Secure = true, HttpOnly = true });
-                    if (!SteamRememberLogin.IsEmpty())
-                        container.Add(url, new Cookie("steamRememberLogin", SteamRememberLogin) { Secure = true });
-                    if (!SteamMachineAuth.IsEmpty())
-                        container.Add(url, new Cookie("steamMachineAuth" + SteamID, SteamMachineAuth));
-                    break;
-            }
-        }
-    }
+		AddCookies(container, url);
+	}
     public string ToStringCookie()
     {
-        string cookie = "";
+        var sb = new StringBuilder(10);
+		sb.Append("Steam_Language=");
+		sb.Append(SteamLanguage);
+		sb.Append("; mobileClientVersion=777777 3.6.4; mobileClient=android; ");
         if (!SessionID.IsEmpty())
-            cookie += $"sessionid={SessionID}; ";
-        if (!SteamLogin.IsEmpty())
-            cookie += $"steamLogin={SteamLogin}; ";
-        if (!SteamLoginSecure.IsEmpty())
-            cookie += $"steamLoginSecure={SteamLoginSecure}; ";
-        if (!WebCookie.IsEmpty())
-            cookie += $"steamMachineAuth{SteamID}={WebCookie}; ";
-        if (cookie == "")
-            return null;
-        cookie += "Steam_Language=english; ";
-        return cookie;
+		{
+			sb.Append("sessionid=");
+			sb.Append(SessionID);
+			sb.Append("; ");
+		}
+		sb.Append("steamLoginSecure=");
+		sb.Append(SteamID);
+		sb.Append("%7C%7C");
+		sb.Append(AccessToken);
+        return sb.ToString();
     }
 
     public void RewriteCookie(CookieContainer container)
@@ -215,11 +128,6 @@ public class SessionData : ISessionProvider
                 cookie.Domain == "help.steampowered.com" || cookie.Domain == "api.steampowered.com")
             {
                 if (cookie.Name == "sessionid") SessionID = cookie.Value;
-                else if (cookie.Name == "steamLoginSecure") SteamLoginSecure = cookie.Value;
-                else if (cookie.Name == "steamRememberLogin") SteamRememberLogin = cookie.Value;
-                else if (cookie.Name.StartsWith("steamMachineAuth")) SteamMachineAuth = cookie.Value;
-                else if (cookie.Name == "steamCountry") SteamCountry = cookie.Value;
-                else if (cookie.Name == "browserid") BrowserID = cookie.Value;
                 else if (cookie.Name == "Steam_Language") SteamLanguage = cookie.Value;
             }
         }
@@ -233,13 +141,10 @@ public class SessionData : ISessionProvider
 				cookie.Domain == "help.steampowered.com" || cookie.Domain == "api.steampowered.com")
 			{
 				if (cookie.Name == "sessionid") SessionID = cookie.Value;
-				else if (cookie.Name == "steamLoginSecure") SteamLoginSecure = cookie.Value;
-				else if (cookie.Name == "steamRememberLogin") SteamRememberLogin = cookie.Value;
-				else if (cookie.Name.StartsWith("steamMachineAuth")) SteamMachineAuth = cookie.Value;
-				else if (cookie.Name == "steamCountry") SteamCountry = cookie.Value;
-				else if (cookie.Name == "browserid") BrowserID = cookie.Value;
 				else if (cookie.Name == "Steam_Language") SteamLanguage = cookie.Value;
 			}
 		}
 	}
+
+	private string GetSteamLoginSecure() => SteamID + "%7C%7C" + AccessToken;
 }
