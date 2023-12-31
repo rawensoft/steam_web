a6946eda0a759f50c9ff0f02631de1f9140d686c
rawensoft <rawensoft@ya.ru>
2023-12-31 18:29:06 +0300

    Исправлены свойства класса для более похожего значения как в steam приложении

--- steam_web/Auth/v2/DTO/AuthSessionResponse.cs
+++ steam_web/Auth/v2/DTO/AuthSessionResponse.cs
@@ -7,10 +7,10 @@ class AuthSessionResponse
 {
     [ProtoMember(1)] public ulong client_id { get; set; }
     [ProtoMember(2)] public byte[] request_id { get; set; }
-    [ProtoMember(3)] public long interval { get; set; }
+    [ProtoMember(3, DataFormat = DataFormat.FixedSize)] public uint interval { get; set; }
     [ProtoMember(4)] public AuthSessionConfirmation[] allowed_confirmations { get; set; } = new AuthSessionConfirmation[0];
     [ProtoMember(5, DataFormat = DataFormat.FixedSize)] public ulong steamid { get; set; }
-    [ProtoMember(6)] public string weak_token { get; set; }
+    [ProtoMember(6)] public string? weak_token { get; set; }
     /// <summary>
     /// if login has been confirmed, may contain remembered machine ID for future login
     /// </summary>
