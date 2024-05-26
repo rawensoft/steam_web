ad5952612d45cebbf5ab5f02fb9adc5554544ca1
rawensoft <rawensoft@ya.ru>
2024-05-26 12:35:24 +0300

    Изменён класс данных Steam Api ключа

--- steam_web/Models/WebApiKey.cs
+++ steam_web/Models/WebApiKey.cs
@@ -3,12 +3,19 @@
 namespace SteamWeb.Models;
 public sealed class WebApiKey
 {
+    /// <summary>
+    /// Api ключа или заглушка в виде большого количества нулей
+    /// </summary>
     public string? Key { get; init; } = null;
     /// <summary>
     /// Domain или ErrorText
     /// </summary>
     public string? Domain { get; init; } = null;
-    public bool IsError => Key == null && Domain != null;
+	/// <summary>
+	/// Не null если <see cref="IsError"/>
+	/// </summary>
+	public string? RawHtml { get; internal set; }
+	public bool IsError => Key == null && Domain != null;
 
     [JsonConstructor]
     public WebApiKey() { }
