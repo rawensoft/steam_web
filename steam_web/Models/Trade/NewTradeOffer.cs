dcfdbe80fc4f5f9d9299a47dff0a2cb5c44adffb
rawensoft <rawensoft@ya.ru>
2024-09-15 15:43:43 +0300

    Изменены типы параметров для создания трейда

--- steam_web/Models/Trade/NewTradeOffer.cs
+++ steam_web/Models/Trade/NewTradeOffer.cs
@@ -14,25 +14,25 @@ public class NewTradeOffer
         [JsonPropertyName("currency")] public List<NewCurrency> Currency { get; init; } = new(2);
         [JsonPropertyName("ready")] public bool Ready { get; init; } = false;
 
-        public void AddAssets(uint appid, string contextid, string assetid, int amount = 1) => Assets.Add(new NewAssets(appid, contextid, assetid, amount));
-        public void AddAssets(uint appid, byte contextid, ulong assetid, int amount = 1) => Assets.Add(new NewAssets(appid, contextid, assetid, amount));
+        public void AddAssets(uint appid, string contextid, string assetid, uint amount = 1) => Assets.Add(new NewAssets(appid, contextid, assetid, amount));
+        public void AddAssets(uint appid, byte contextid, ulong assetid, uint amount = 1) => Assets.Add(new NewAssets(appid, contextid, assetid, amount));
     }
     public class NewAssets
     {
         [JsonPropertyName("appid")] public uint AppId { get; init; }
         [JsonPropertyName("contextid")] public string ContextId { get; init; }
-        [JsonPropertyName("amount")] public int Amount { get; init; }
+        [JsonPropertyName("amount")] public uint Amount { get; init; }
         [JsonPropertyName("assetid")] public string AssetId { get; init; }
 
         [JsonConstructor]
-        public NewAssets(uint AppId, string ContextId, string AssetId, int Amount = 1)
+        public NewAssets(uint AppId, string ContextId, string AssetId, uint Amount = 1)
         {
             this.AppId = AppId;
             this.ContextId = ContextId;
             this.Amount = Amount;
             this.AssetId = AssetId;
         }
-        public NewAssets(uint AppId, byte ContextId, ulong AssetId, int Amount = 1)
+        public NewAssets(uint AppId, byte ContextId, ulong AssetId, uint Amount = 1)
         {
             this.AppId = AppId;
             this.ContextId = ContextId.ToString();
