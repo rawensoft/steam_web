8019eb29cb68a53ba405a8fbc2ab995a65f0a746
rawensoft <rawensoft@ya.ru>
2024-09-16 23:22:38 +0300

    Добавлен новый набор api методов ISteamApps

---
+++ steam_web/API/ISteamApps.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json;
+using SteamWeb.API.Models;
+using SteamWeb.API.Models.ISteamApps;
+using SteamWeb.Web;
+
+namespace SteamWeb.API;
+public static class ISteamApps
+{
+    public static ResponseData<UpToDateModel> UpToDateCheck(ApiRequest apiRequest, uint appid, uint version)
+    {
+        var request = new PostRequest(SteamPoweredUrls.ISteamApps_UpToDateCheck_v1, Downloader.AppFormUrlEncoded)
+        {
+            Proxy = apiRequest.Proxy,
+            CancellationToken = apiRequest.CancellationToken,
+        };
+        apiRequest.AddAuthToken(request).AddPostData("appid", appid).AddPostData("version", version);
+        var response = Downloader.Post(request);
+        if (!response.Success)
+            return new();
+        try
+        {
+            var obj = JsonSerializer.Deserialize<ResponseData<UpToDateModel>>(response.Data!, Steam.JsonOptions)!;
+            obj.Success = true;
+            return obj;
+        }
+        catch (Exception)
+        {
+            return new();
+        }
+    }
+    public static async Task<ResponseData<UpToDateModel>> UpToDateCheckAsync(ApiRequest apiRequest, uint appid, uint version)
+    {
+        var request = new PostRequest(SteamPoweredUrls.ISteamApps_UpToDateCheck_v1, Downloader.AppFormUrlEncoded)
+        {
+            Proxy = apiRequest.Proxy,
+            CancellationToken = apiRequest.CancellationToken,
+        };
+        apiRequest.AddAuthToken(request).AddPostData("appid", appid).AddPostData("version", version);
+        var response = await Downloader.PostAsync(request);
+        if (!response.Success)
+            return new();
+        try
+        {
+            var obj = JsonSerializer.Deserialize<ResponseData<UpToDateModel>>(response.Data!, Steam.JsonOptions)!;
+            obj.Success = true;
+            return obj;
+        }
+        catch (Exception)
+        {
+            return new();
+        }
+    }
+}
---
+++ steam_web/API/Models/ISteamApps/UpToDateModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SteamWeb.API.Models.ISteamApps;
+public class UpToDateModel
+{
+    [JsonPropertyName("success")] public bool Success { get; init; } = false;
+    [JsonPropertyName("up_to_date")] public bool UpToDate { get; init; } = false;
+    [JsonPropertyName("version_is_listable")] public bool VersionIsListable { get; init; } = false;
+    [JsonPropertyName("required_version")] public uint RequiredVersion { get; init; }
+    [JsonPropertyName("message")] public string? Message { get; init; }
+}
