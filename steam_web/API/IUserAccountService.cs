1b1c90d41bb04fcdb9c68f54a571fe4d084f82fb
rawensoft <rawensoft@ya.ru>
2024-09-16 23:23:48 +0300

    Добавлен новый набор api методов IUserAccountService

---
+++ steam_web/API/IUserAccountService.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.Json;
+using SteamWeb.API.Models;
+using SteamWeb.API.Models.IUserAccountService;
+using SteamWeb.Web;
+
+namespace SteamWeb.API;
+public static class IUserAccountService
+{
+    public static ResponseData<ClientWalletDetails> GetClientWalletDetails(ApiRequest apiRequest,
+        bool? include_balance_in_usd = null, int? wallet_region = null, bool? include_formatted_balance = null)
+    {
+        var request = new PostRequest(SteamPoweredUrls.IUserAccountService_GetClientWalletDetails_v1, Downloader.AppFormUrlEncoded)
+        {
+            Proxy = apiRequest.Proxy,
+            CancellationToken = apiRequest.CancellationToken,
+            UserAgent = KnownUserAgents.SteamWindowsClient,
+        };
+        apiRequest.AddAuthToken(request);
+        if (include_balance_in_usd.HasValue)
+            request.AddPostData("include_balance_in_usd", include_balance_in_usd.Value);
+        if (wallet_region.HasValue)
+            request.AddPostData("wallet_region", wallet_region.Value);
+        if (include_formatted_balance.HasValue)
+            request.AddPostData("include_formatted_balance", include_formatted_balance.Value);
+
+        var response = Downloader.Post(request);
+        if (!response.Success)
+            return new();
+        try
+        {
+            var obj = JsonSerializer.Deserialize<ResponseData<ClientWalletDetails>>(response.Data!, Steam.JsonOptions)!;
+            obj.Success = true;
+            return obj;
+        }
+        catch (Exception)
+        {
+            return new();
+        }
+    }
+    public static async Task<ResponseData<ClientWalletDetails>> GetClientWalletDetailsAsync(ApiRequest apiRequest,
+        bool? include_balance_in_usd = null, int? wallet_region = null, bool? include_formatted_balance = null)
+    {
+        var request = new PostRequest(SteamPoweredUrls.IUserAccountService_GetClientWalletDetails_v1, Downloader.AppFormUrlEncoded)
+        {
+            Proxy = apiRequest.Proxy,
+            CancellationToken = apiRequest.CancellationToken,
+            UserAgent = KnownUserAgents.SteamWindowsClient,
+        };
+        apiRequest.AddAuthToken(request);
+        if (include_balance_in_usd.HasValue)
+            request.AddPostData("include_balance_in_usd", include_balance_in_usd.Value);
+        if (wallet_region.HasValue)
+            request.AddPostData("wallet_region", wallet_region.Value);
+        if (include_formatted_balance.HasValue)
+            request.AddPostData("include_formatted_balance", include_formatted_balance.Value);
+
+        var response = await Downloader.PostAsync(request);
+        if (!response.Success)
+            return new();
+        try
+        {
+            var obj = JsonSerializer.Deserialize<ResponseData<ClientWalletDetails>>(response.Data!, Steam.JsonOptions)!;
+            obj.Success = true;
+            return obj;
+        }
+        catch (Exception)
+        {
+            return new();
+        }
+    }
+}
---
+++ steam_web/API/Models/IUserAccountService/ClientWalletDetails.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SteamWeb.API.Models.IUserAccountService;
+public class ClientWalletDetails
+{
+    [JsonPropertyName("has_wallet")] public bool HasWallet { get; init; } = false;
+    [JsonPropertyName("user_country_code")] public string? UserCountryCode { get; init; }
+    [JsonPropertyName("wallet_country_code")] public string? WalletCountryCode { get; init; }
+    [JsonPropertyName("wallet_state")] public string WalletState { get; init; } = string.Empty;
+    [JsonPropertyName("balance")] public uint Balance { get; init; }
+    [JsonPropertyName("delayed_balance")] public uint DelayedBalance { get; init; }
+    [JsonPropertyName("currency_code")] public byte CurrencyCode { get; init; }
+    [JsonPropertyName("time_most_recent_txn")] public int TimeMostRecentTxn { get; init; }
+    [JsonPropertyName("most_recent_txnid")] public string? MostRecentTxnId { get; init; }
+    [JsonPropertyName("balance_in_usd")] public uint BalanceInUsd { get; init; }
+    [JsonPropertyName("delayed_balance_in_usd")] public uint DelayedBalanceInUsd { get; init; }
+    [JsonPropertyName("has_wallet_in_other_regions")] public bool HasWalletInOtherRegions { get; init; } = false;
+    [JsonPropertyName("formatted_balance")] public string? FormattedBalance { get; init; }
+    [JsonPropertyName("formatted_delayed_balance")] public string? FormattedDelayedBalance { get; init; }
+    [JsonPropertyName("other_regions")] public byte[] OtherRegions { get; init; } = Array.Empty<byte>();
+}
