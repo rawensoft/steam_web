05efb251e64d734567c8749406ea9bf0e1d97ee1
rawensoft <rawensoft@ya.ru>
2024-09-22 17:36:38 +0300

    Добавлены get свойства для обнаружения стандартных способов покупки

--- steam_web/Models/PurchaseHistory/PurchasePaymentMethodModel.cs
+++ steam_web/Models/PurchaseHistory/PurchasePaymentMethodModel.cs
@@ -1,8 +1,14 @@
-﻿namespace SteamWeb.Models.PurchaseHistory;
+﻿using System.Text.Json.Serialization;
+
+namespace SteamWeb.Models.PurchaseHistory;
 public class PurchasePaymentMethodModel
 {
     private const char _tabCh = '\t';
     private const string _newLineStr = "\n";
+    private const string _wallet = "Wallet";
+    private const string _walletCredit = "Wallet Credit";
+    private const string _retail = "Retail";
+    private const string _kiosk = "Kiosk";
 
     /// <summary>
     /// Отображает какой метод оплаты был использован при использовании этой покупки
@@ -12,6 +18,9 @@ public class PurchasePaymentMethodModel
     /// Указывается сумма, если было несколько методов оплаты, иначе null
     /// </summary>
     public string? Amount { get; init; }
+    [JsonIgnore] public bool IsWalletMethod => Method == _wallet || Method == _walletCredit;
+    [JsonIgnore] public bool IsRetailMethod => Method == _retail;
+    [JsonIgnore] public bool IsKioskMethod => Method == _kiosk;
 
     internal static PurchasePaymentMethodModel Parse(string textContent)
     {
