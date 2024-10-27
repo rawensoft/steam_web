fff9f03dea5bb77aebbe93c31c4010007dbe1551
rawensoft <rawensoft@ya.ru>
2024-10-27 16:36:28 +0300

    Добавлен IEquatable интерфейс и рефакторинг свойств класса

--- steam_web/Script/DTO/Historing/HistoryItem.cs
+++ steam_web/Script/DTO/Historing/HistoryItem.cs
@@ -1,33 +1,30 @@
 ﻿using SteamWeb.Script.Enums;
 
 namespace SteamWeb.Script.DTO.Historing;
-
-public record HistoryItem
+public sealed class HistoryItem: IEquatable<HistoryItem>
 {
     public TYPE_HISTORY_ITEM Type { get; internal set; }
     public string Name { get; internal set; }
-    public string Your_Price { get; internal set; }
+    public string? Your_Price { get; internal set; }
     public string Buyer_Username { get; internal set; }
-    public string Buyer_URL { get; internal set; }
-    public string ListedOn { get; internal set; }
-    public string ActedOn { get; internal set; }
+    public string? Buyer_URL { get; internal set; }
+    public string? ListedOn { get; internal set; }
+    public string? ActedOn { get; internal set; }
     public string Game { get; internal set; }
-    public string RemoveID0 { get; internal set; }
-    public string RemoveID1 { get; internal set; }
-    public double GetYour_Price
+    public string? RemoveID0 { get; internal set; }
+    public string? RemoveID1 { get; internal set; }
+    public decimal GetYour_Price
     {
         get
         {
-            if (string.IsNullOrEmpty(Your_Price)) return 0;
+            if (string.IsNullOrEmpty(Your_Price))
+                return 0;
             string splitted = Your_Price.Split(' ')[0].Replace(".", ",");
-            if (double.TryParse(splitted, out var result)) return result;
+            if (decimal.TryParse(splitted, out var result))
+                return result;
             return 0;
         }
     }
 
-    public bool Compare(string RemoveID)
-    {
-        if (RemoveID0 == RemoveID || RemoveID1 == RemoveID) return true;
-        return false;
-    }
+    public bool Equals(HistoryItem? other) => RemoveID0 == other?.RemoveID0 || RemoveID1 == other?.RemoveID1;
 }
