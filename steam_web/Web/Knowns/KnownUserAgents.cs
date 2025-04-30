4158f1138e5fa70277c29bdad536417ba0d80ed7
rawensoft <rawensoft@ya.ru>
2025-04-30 12:28:53 +0300

    Добавлен новый UserAgent SteamOverlayWebBrowser

--- steam_web/Web/Knowns/KnownUserAgents.cs
+++ steam_web/Web/Knowns/KnownUserAgents.cs
@@ -6,4 +6,5 @@ public static class KnownUserAgents
     public const string SteamMobileBrowser = "Dalvik/2.1.0 (Linux; U; Android 5.1.1; ASUS_Z01QD Build/QKQ1.190825.002; Valve Steam App Version/3)";
     public const string WindowsBrowser = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36";
     public const string SteamWindowsClient = "Mozilla/5.0 (Windows; U; Windows NT 10.0; en-US; Valve Steam Client/default/1607131459; ) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36";
+    public const string SteamWindowsClientOverlay = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; Valve Steam GameOverlay/default/0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.183 Safari/537.36";
 }
