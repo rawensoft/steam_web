e25d7718c1fc1d52af0cc9ad98e8a4be38e30cbc
rawensoft <rawensoft@ya.ru>
2025-11-01 19:06:52 +0300

    Добавлен список с некоторыми методами пополнения

---
+++ steam_web/Models/PurchaseHistory/PurchasePaymentMethods.cs
@@ -0,0 +1,25 @@
+﻿namespace SteamWeb.Models.PurchaseHistory;
+
+public static class PurchasePaymentMethods
+{
+    public const string Wallet = "Wallet";
+    public const string WalletCredit = "Wallet Credit";
+    public const string Retail = "Retail";
+    public const string Kiosk = "Kiosk";
+    public const string PayPal = "PayPal";
+    public const string Yandex = "Yandex";
+    public const string QIWI = "QIWI Wallet";
+    public const string WebMoney = "WebMoney";
+    public const string VISA = "Visa";
+    public const string PaySafeCard = "PaySafeCard";
+    public const string MasterCard = "MasterCard";
+    public const string Mir = "Mir";
+    public const string MobilePayments = "Mobile Payments";
+    public const string AliPay = "AliPay";
+    public const string WeChat = "WeChat";
+    public const string Konbini = "Konbini"; //some japan method
+    public const string CultureVoucher = "Culture Voucher"; //some korea method
+    public const string Sofortuberweisung = "Sofortüberweisung"; //some germany method
+    public const string Skrill = "Skrill";
+    public const string Trustly = "Trustly";
+}
