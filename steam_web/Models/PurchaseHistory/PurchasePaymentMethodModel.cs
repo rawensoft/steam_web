b004cc12d30ad5e057f2c924636fcb8e674dc7d5
rawensoft <rawensoft@ya.ru>
2025-11-01 19:05:23 +0300

    Улучшено определение методов пополнения в истории покупок

--- steam_web/Models/PurchaseHistory/PurchasePaymentMethodModel.cs
+++ steam_web/Models/PurchaseHistory/PurchasePaymentMethodModel.cs
@@ -9,11 +9,12 @@ public class PurchasePaymentMethodModel
     private const string _walletCredit = "Wallet Credit";
     private const string _retail = "Retail";
     private const string _kiosk = "Kiosk";
+    private const string _payPal = "PayPal";
 
     /// <summary>
     /// Отображает какой метод оплаты был использован при использовании этой покупки
     /// </summary>
-    public string Method { get; init; }
+    public required string Method { get; init; }
     /// <summary>
     /// Указывается сумма, если было несколько методов оплаты, иначе null
     /// </summary>
@@ -21,6 +22,7 @@ public class PurchasePaymentMethodModel
     [JsonIgnore] public bool IsWalletMethod => Method == _wallet || Method == _walletCredit;
     [JsonIgnore] public bool IsRetailMethod => Method == _retail;
     [JsonIgnore] public bool IsKioskMethod => Method == _kiosk;
+    [JsonIgnore] public bool IsPayPalMethod => Method == _payPal;
 
     internal static PurchasePaymentMethodModel Parse(string textContent)
     {
@@ -35,7 +37,14 @@ public class PurchasePaymentMethodModel
                     amount = item;
                 else
                 {
-                    method = item;
+                    if (item.StartsWith(PurchasePaymentMethods.MasterCard, StringComparison.OrdinalIgnoreCase))
+                        method = PurchasePaymentMethods.MasterCard;
+                    else if (item!.StartsWith(PurchasePaymentMethods.VISA, StringComparison.OrdinalIgnoreCase))
+                        method = PurchasePaymentMethods.VISA;
+                    else if (item!.StartsWith(PurchasePaymentMethods.Mir, StringComparison.OrdinalIgnoreCase))
+                        method = PurchasePaymentMethods.Mir;
+                    else
+                        method = item;
                     break;
                 }
             }
